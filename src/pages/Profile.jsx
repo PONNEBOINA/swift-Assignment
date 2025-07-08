5600f514655e558ac94fb2cc711e20d45ef56a8a
ponneboina <vamshiyadav406@gmail.com>
2025-07-08 18:07:18 +0530

    first commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Profile.css';
+import './profile.css';
 
 const Profile = () => {
   const [user, setUser] = useState(null);
